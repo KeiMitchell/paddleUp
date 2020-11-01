12cadb61bf2df0f1c39643520ea13f5da2a30dc9
KeiMichell <krockdale611@gmail.com>
2020-11-01 16:09:20 +0900

    船投稿機能

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -2,4 +2,17 @@ class BoatsController < ApplicationController
   def index
     @boats = Boat.all
   end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    Boat.create(boat_params)
+  end
+
+  private
+  def boat_params
+    params.require(:boat).permit(:image, :description, :brand_id, :size_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "boats#index"
-  resources :boats, only: :index
+  resources :boats, only: [:index, :new, :create]
 end
