56f7dfb35b435d1f5e55cf19476e90803705c990
KeiMichell <krockdale611@gmail.com>
2020-11-03 10:56:48 +0900

    add user_id column to Boats table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_050000) do
+ActiveRecord::Schema.define(version: 2020_11_03_015548) do
 
   create_table "boats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_050000) do
     t.integer "size_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
