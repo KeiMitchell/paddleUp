4b2eea9b68fb8148ea0a97ae2c99eaded093cc49
KeiMichell <krockdale611@gmail.com>
2020-11-04 12:27:21 +0900

    検索結果の表示
    ・部分テンプレートの導入
    ・localsオプションをつけることで部分テンプレート内で指定した変数を使用することができるようになる！

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,7 +4,7 @@ class Boat < ApplicationRecord
 
   def self.search(search) #引数は入力された文字
     if search != "" #もし検索が空欄でなければ
-      Boat.where('text LIKE(?)', "%#{search}%")
+      Boat.where('description LIKE(?)', "%#{search}%")
     else            #もし検索が空欄であれば全て取得する
       Boat.all
     end
