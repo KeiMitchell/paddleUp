f1118a279f8e4bfcd055f50f993946938d41c144
KeiMichell <krockdale611@gmail.com>
2020-11-04 10:37:28 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "boats#index"
   resources :boats do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
