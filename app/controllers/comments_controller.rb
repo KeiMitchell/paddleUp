526a85bcfb1e800cc224557c6513241c5a0a2305
KeiMichell <krockdale611@gmail.com>
2020-11-04 10:56:15 +0900

    リダイレクト画面の設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment_params)
+    redirect_to "/boats/#{comments.boat.id}" #redirect to boat詳細画面
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, boat_id: params[:boat_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, boat_id: params[:boat_id]) #ストロングパラメーターの設定
   end
 end
