f11b832601c76ec271340af62ed39482598faac8
KeiMichell <krockdale611@gmail.com>
2020-11-06 13:43:19 +0900

    boat.rb edit

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,8 @@
 class Boat < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   mount_uploader :image, ImageUploader
   belongs_to :user
+  belongs_to_active_hash :brand
   has_many :comments
 
   def self.search(search) #引数は入力された文字
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ActiveHash::Base
     self.data = [
-      {id: 1, name: 'Nelo'}, {id: 2, name: 'Plastex'}, {id: 3, name: 'Elio'}, {id: 4, name: 'Vadja'}, {id: 5, name: 'Epic'}, {id: 6, name'Struer Kajak'}, {id: 7, name: 'Hikari'}
+      {id: 1, name: 'Nelo'}, {id: 2, name: 'Plastex'}, {id: 3, name: 'Elio'}, {id: 4, name: 'Vadja'}, {id: 5, name: 'Epic'}, {id: 6, name: 'Struer Kajak'}, {id: 7, name: 'Hikari'}
     ]
   end
